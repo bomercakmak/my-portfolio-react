ef54fa68d2e5499b382723047c7b26d5eb4ef8f0
Bahadir Omer Cakmak <bahadir0646@gmail.com>
2021-03-25 20:02:03 +0100

    I added navbar elements. I also write some scss for navbar.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.scss';
+import NavBar from './Components/NavBar'
 
 function App() {
   return (
     <div className="App">
         <div className="sidebar">
+          <NavBar/>
         </div>
         <div className="main-content"></div>
     </div>
---
+++ src/Components/NavBar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import avatar from "../img/avatar.jpg";
+import { NavLink } from "react-router-dom";
+function NavBar() {
+  return (
+    <div className="NavBar">
+      <nav className="nav">
+        <div className="profile">
+          <img src={avatar} alt="" />
+        </div>
+        <ul className="nav-items">
+          <li className="nav-item">
+            <NavLink to="/" exact activeClassName="active">
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/about" exact activeClassName="active">
+              About
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/projects" exact activeClassName="active">
+              Projects
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/blogs" exact activeClassName="active">
+              Blogs
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/contact" exact activeClassName="active">
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+        <footer className="footer">
+          <p>@2021 Bahadir Omer Cakmak</p>
+        </footer>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;
