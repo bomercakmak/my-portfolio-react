0b2e081fcff4405891c850e16c425d5e259a393f
Bahadir Omer Cakmak <bahadir0646@gmail.com>
2021-03-25 19:28:16 +0100

    I added layout sidebar and main-content scss. I also install react-router-dom.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div >
-      
+    <div className="App">
+        <div className="sidebar">
+        </div>
+        <div className="main-content"></div>
     </div>
   );
 }
