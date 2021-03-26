890b2c14596bc781f36fc31a9bd247f64a245ad3
Bahadir Omer Cakmak <bahadir0646@gmail.com>
2021-03-26 15:28:38 +0100

    I added Homepage information and I also added @fortawesome/free-brands-svg-icons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import './App.scss';
-import NavBar from './Components/NavBar'
+import "./App.scss";
+import NavBar from "./Components/NavBar";
+import HomePage from "./Pages/HomePage";
 
 function App() {
   return (
     <div className="App">
-        <div className="sidebar">
-          <NavBar/>
+      <div className="sidebar">
+        <NavBar />
+      </div>
+      <div className="main-content">
+        <div className="content">
+          <HomePage />
         </div>
-        <div className="main-content"></div>
+      </div>
     </div>
   );
 }
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,16 +1,44 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faGithub,
+  faLinkedin,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
+import { Link } from "react-router-dom";
 
 function HomePage() {
-    return (
-        <div className="HomePage">
-            <header className="hero">
-                <h1 className="hero-text">
-                    Hi, I am 
-                    <span>Bahadir Omer Cakmak</span>
-                </h1>
-            </header>
+  return (
+    <div className="HomePage">
+      <header className="hero">
+        <h1 className="hero-text">
+          Hi, I am
+          <span> Bahadir Omer Cakmak</span>
+        </h1>
+        <p className="h-sub-text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis
+          arcu et finibus bibendum. Maecenas scelerisque varius sodales. Morbi
+          dapibus ultricies velit, eu scelerisque nisi molestie eget. Vestibulum
+          ante ipsum.
+        </p>
+        <div className="icons">
+          <Link>
+            <FontAwesomeIcon icon={faGithub} />
+          </Link>
+          <Link>
+            <FontAwesomeIcon icon={faLinkedin} />
+          </Link>
+          <Link>
+            <FontAwesomeIcon icon={faWhatsapp} />
+          </Link>
+          <Link>
+            <FontAwesomeIcon icon={faFacebook} />
+          </Link>
         </div>
-    )
+      </header>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
