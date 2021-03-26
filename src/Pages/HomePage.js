fa5f1197df4184d45d0ca36cfd5b9a84bd0ac1af
Bahadir Omer Cakmak <bahadir0646@gmail.com>
2021-03-26 14:56:12 +0100

    I added transition for nav-bar items.

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 function HomePage() {
     return (
-        <div>
-            <h1>HomePage</h1>
+        <div className="HomePage">
+            <header className="hero">
+                <h1 className="hero-text">
+                    Hi, I am 
+                    <span>Bahadir Omer Cakmak</span>
+                </h1>
+            </header>
         </div>
     )
 }
