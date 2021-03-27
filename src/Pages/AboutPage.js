f4da3f21875eecfe414d8d527dd08adf88ae6972
Bahadir Omer Cakmak <bahadir0646@gmail.com>
2021-03-27 13:11:01 +0100

    I added About page images and iformations.

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Title from '../Components/Title'
-
+import ImageSection from './ImageSection'
 function AboutPage() {
     return (
         <div className="AboutPage">
             <Title title={"About Me"} span={"About Me"}/>
+            <ImageSection/>
         </div>
     )
 }
--- src/Pages/ImageSection.js
+++ src/Pages/ImageSection.js
@@ -1,13 +1,26 @@
-import React from 'react'
-
+import React from "react";
+import about from "../img/about.jpg";
 function ImageSection() {
-    return (
-        <div className="ImageSection">
-            <div className="img">
-                <img src={about} alt=""/>
-            </div>
+  return (
+    <div className="ImageSection">
+      <div className="img">
+        <img src={about} alt="" />
+      </div>
+      <div className="about-info">
+        <h4>
+          I am <span>Bahadir Omer Cakmak</span>
+        </h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis
+          arcu et finibus bibendum. Maecenas scelerisque varius sodales. Morbi
+          dapibus ultricies velit, eu scelerisque nisi molestie eget.
+        </p>
+        <div className="about-details">
+         
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default ImageSection
+export default ImageSection;
