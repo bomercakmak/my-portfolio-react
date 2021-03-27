49f76a17a775e97afdb6635cb65fcc060c051f99
Bahadir Omer Cakmak <bahadir0646@gmail.com>
2021-03-27 13:56:34 +0100

    I added my about-details section and I added CV button.

--- src/Pages/ImageSection.js
+++ src/Pages/ImageSection.js
@@ -10,14 +10,33 @@ function ImageSection() {
         <h4>
           I am <span>Bahadir Omer Cakmak</span>
         </h4>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis
-          arcu et finibus bibendum. Maecenas scelerisque varius sodales. Morbi
-          dapibus ultricies velit, eu scelerisque nisi molestie eget.
+        <p className="about-text">
+          Web Developer with 2 years of experience. Focused on expanding
+          knowledge and abilities in website design, programming and quality
+          assurance. Computer Engineering student. Especially specialized in Web
+          Technologies Engineering such as HTML5, CSS, Bootstrap, JavaScript,
+          React, Redux, Firebase. Excellent communication and organizational
+          abilities with resourceful approach to solving diverse problems.
         </p>
         <div className="about-details">
-         
+          <div className="left-section">
+            <p>Full Name</p>
+            <p>Age</p>
+            <p>Nationality</p>
+            <p>Languages</p>
+            <p>Hobbies</p>
+            <p>Address</p>
+          </div>
+          <div className="right-section">
+            <p>: Bahadir Omer Cakmak</p>
+            <p>: 21</p>
+            <p>: Turkish</p>
+            <p>: Turkish, English, German, Azerbaijani</p>
+            <p>: Table Tennis, Play Chess, Hiking</p>
+            <p>: Bonn Germany</p>
+          </div>
         </div>
+        <button className="btn">Download My CV</button>
       </div>
     </div>
   );
