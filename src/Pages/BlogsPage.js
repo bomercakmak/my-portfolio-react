7472db7059a59bf23a9f758d85fe0abdb1d1d455
Bahadir Omer Cakmak <bahadir0646@gmail.com>
2021-03-28 15:06:41 +0200

    I added some example blog.

--- src/Pages/BlogsPage.js
+++ src/Pages/BlogsPage.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import allBlogs from '../Components/allBlogs'
 
 function BlogsPage() {
     return (
-        <div>
-            <h1>Blogs Page</h1>
+        <div className="BlogsPage">
+            {
+            allBlogs.map((blog) => {
+                return <div className="blog" key={blog.id}>
+                    <div className="blog-content">
+                        <img src={blog.image} alt=''/>
+                        <a href={blog.link} className="blog-link">
+                            {blog.title}
+                        </a>
+                        <div className="date-icon">
+                            
+                        </div>
+                    </div>
+
+                </div>
+               
+            })
+            }
         </div>
     )
 }
