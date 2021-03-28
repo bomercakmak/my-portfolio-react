1d1019a7116861404592b89ede1a7f22226ddf6b
Bahadir Omer Cakmak <bahadir0646@gmail.com>
2021-03-28 21:11:22 +0200

    I added contact page information. I also added google maps iframe.

--- src/Components/ContactItem.js
+++ src/Components/ContactItem.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
-function ContactItem({icon, text1, text2}) {
+function ContactItem({icon, text1, text2,title}) {
     return (
-        <div>
+        <div className="ContactItem">
             <div className="contact">
-                <img src={icon} alt={}/>
+                <img src={icon} alt=''/>
                 <div className="right-items">
-                    
+                 <h6>{title}</h6>   
+                 <p>{text1}</p>   
+                 <p>{text2}</p>   
                 </div>
             </div>
         </div>
--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -7,12 +7,12 @@ function ContactPage() {
     return (
         <div  className="ContactPage">
            <div className="map-sect">
-
+           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d149684.61726839762!2d7.0478417793708505!3d50.67306913881625!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47bee19f7ccbda49%3A0x86dbf8c6685c9617!2sBonn!5e0!3m2!1str!2sde!4v1616957526013!5m2!1str!2sde" width="600" height="450" style={{border:0}} allowFullScreen="" loading="lazy"></iframe>
            </div>
            <div className="contact-sect">
-               <ContactItem icon={phone} text1={"+48 517 220 852"} text2={"+213 1232 2132"}/>
-               <ContactItem icon={email} text1={"+48 517 220 852"} text2={"+213 1232 2132"}/>
-               <ContactItem icon={location} text1={"+48 517 220 852"} text2={"+213 1232 2132"}/>
+               <ContactItem icon={phone} text1={"+48 517 220 852"} text2={"+213 1232 2132"} title="Phone"/>
+               <ContactItem icon={email} text1={"+48 517 220 852"} text2={"+213 1232 2132"} title="Email"/>
+               <ContactItem icon={location} text1={"+48 517 220 852"} text2={"+213 1232 2132"} title="Address"/>
            </div>
         </div>
     )
