7b6d4d0d507fc2c96a0eb7f6ae4282c0edcc78c6
Bahadir Omer Cakmak <bahadir0646@gmail.com>
2021-04-02 17:01:55 +0200

    I added filtered func.

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
-function Categories() {
+function Categories({filter,categories}) {
     return (
         <div>
-            
+            {
+                categories.map((cat,i)=>{
+                    return <button key={i} onClick={()=> filter(cat)}>{cat}</button>
+                })
+            }
         </div>
     )
 }
--- src/Pages/ProjectsPage.js
+++ src/Pages/ProjectsPage.js
@@ -4,17 +4,28 @@ import Categories from "../Components/Categories";
 import MenuItems from "../Components/MenuItems";
 import projects from "../Components/allProjects";
 
+const allCategories = ['All', ...new Set(projects.map(item => item.category))]
+
 
 function ProjectsPage() {
-    const [categories, setCategories] = useState(null)
+    const [categories, setCategories] = useState(allCategories)
     const [menuItems,setMenuItems] = useState(projects)
+
+    const filter = (category) => {
+        const filteredData = projects.filter((item) =>{
+          return item.category === category
+        })
+        setMenuItems(filteredData)
+    }
+
+
   return (
     <div className="ProjectPage">
       <div className="title">
         <Title title={"Projects"} span={"Projects"} />
       </div>
       <div className="projects-data">
-        <Categories />
+        <Categories filter={filter} categories={categories}/>
         <MenuItems menuItems={menuItems}/>
       </div>
     </div>
