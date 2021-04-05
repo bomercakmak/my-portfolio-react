ccf664e16390663ce18510a4725648567267eeab
Bahadir Omer Cakmak <bahadir0646@gmail.com>
2021-04-05 20:17:40 +0200

    I added some projects.

--- src/Components/allProjects.js
+++ src/Components/allProjects.js
@@ -41,13 +41,57 @@ const projects = [
     {
         id: 4,
         category: "React JS",
-        link1: 'https://github.com/bomercakmak/task-manager-react',
-        link2: 'https://static-task-manager-react.netlify.app/',
+        link1: 'https://github.com/bomercakmak/my-movies-react',
+        link2: '',
         icon1: 'SOURCE \n CODE',
         icon2: '',
         image: taskManager,
         title: 'My Movies React JS',
         info:"HTML5, CSS3, Bootstrap, JavaScript, JSON Server REST API, React JS, React Router,"
+    },
+    {
+        id: 6,
+        category: "ASP.NET",
+        link1: 'https://github.com/bomercakmak/food-recipes-ASP.NET',
+        link2: '',
+        icon1: 'SOURCE \n CODE',
+        icon2: '',
+        image: taskManager,
+        title: 'Food Recipes Website ASP.NET',
+        info:"HTML5, CSS3, C#, ASP.NET, Microsoft SQL Server"
+    },
+    {
+        id: 5,
+        category: "React JS",
+        link1: 'https://github.com/bomercakmak/employee-management-system-react',
+        link2: 'https://bomercakmak-employee-management-system.netlify.app/',
+        icon1: 'SOURCE \n CODE',
+        icon2: 'LIVE \n WEBSITE',
+        image: taskManager,
+        title: 'Employee Management System React JS',
+        info:"HTML5, CSS3, JavaScript, Web Storage API, React JS"
+    },
+    {
+        id: 7,
+        category: "React JS",
+        link1: 'https://github.com/bomercakmak/youtube-mp4-mp3-downloader-converter-react',
+        link2: 'https://bomercakmak-youtube-mp3-mp4-downloader.netlify.app/',
+        icon1: 'SOURCE \n CODE',
+        icon2: 'LIVE \n WEBSITE',
+        image: taskManager,
+        title: 'Youtube MP4/MP3 Downloader Converter React JS',
+        info:"HTML5, CSS3, JavaScript, React JS"
+    },
+    {
+        id: 8,
+        category: "Express JS",
+        link1: 'https://github.com/bomercakmak/SO-Question-Answer-Rest-Api',
+        link2: '',
+        icon1: 'SOURCE \n CODE',
+        icon2: '',
+        image: taskManager,
+        title: 'Stack Overflow Question Answer Backend Clone',
+        info:"JavaScript, Node.js, Express, MongoDB, Mongoose, JWT, Multer, Nodemailer"
     },
    
 ]
