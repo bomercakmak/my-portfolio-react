dc84e41e3fc49c65ad2aa00c542cdde40176a3ae
Bahadir Omer Cakmak <bahadir0646@gmail.com>
2021-04-09 12:57:18 +0200

    I create reusable experience components.

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -3,16 +3,16 @@ import Container from "react-bootstrap/Container";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Card from "react-bootstrap/Card";
 import Tilt from "react-tilt";
-import hereMood from "../img/experience/Here.png"
+import hereMood from "../img/experience/Here.png";
 import "../styles/experience.scss";
 
-function Experience() {
+  function Experience({img,title,tech,duration,location,description}) {
   return (
     <div id="experience">
-      <h1 className="pt-3 text-center font-details-b pb-3">EXPERIENCE</h1>
+      <h1 className="pt-3 text-center font-details-b pb-3"></h1>
       <Jumbotron className="jumbo-style">
         <Container>
-          <Tilt options={{ max: 25 }}>
+          <Tilt options={{ max: 10 }}>
             <Card className="experience-card">
               <Card.Header
                 as="h5"
@@ -21,51 +21,41 @@ function Experience() {
                 <Card.Img
                   variant="top"
                   className="img-resize"
-                  src={hereMood}
+                  src={img}
                   alt="Accenture logo"
                 />
               </Card.Header>
               <Card.Body className="d-flex justify-content-center flex-column">
                 <div>
                   <Card.Title className="text-center">
-                    Associate Software Engineer
+                  {title}
                   </Card.Title>
                 </div>
                 <div>
                   <Card.Text className="text-center style">
-                    <strong className="body-title-style ">
-                      Full Stack Developer
+                    {/* <strong className="body-title-style ">
+                    
                     </strong>
+                    <br /> */}
+                    <strong>Technology:</strong> Wordpress, Shopify, BigCommerce, Adobe Photoshop, Adobe Premiere Pro, DaVinci Resolve and Microsoft Office 365
                     <br />
-                    <strong>Technology:</strong> React JS, DOTNET, C#, MS-SQL,
-                    Python
+                    <strong>Duration:</strong> August 2018 - June 2019
                     <br />
-                    <strong>Duration:</strong> June 2019 - Present
+                    <strong>Location:</strong> Cologne / Germany
                     <br />
-                    <strong> Description </strong>
-                    <ul className="text-left">
-                      <li>
-                        <strong>Developed &amp; enhanced</strong> multiple
-                        features with customizability option across web
-                        application.
-                      </li>
-                      <li>
-                        <strong>Developed</strong> automation system to create
-                        SQL bulk query scripts that increased efficiency by 80%
-                        and decreased working hours from 4 hours to 30 mins per
-                        task.
-                      </li>
-                      <li>
-                        <strong>Provided</strong> application maintenance while
-                        working as `Production Support`.
-                      </li>
-                      <li>
-                        <strong>Performed</strong> CRUD operations on multiple
-                        databases to load/ remove data according to the business
-                        requirements.
-                      </li>
-                      {/* <li><strong>Co-created</strong> React Document used as a guide for new developers.</li> */}
-                    </ul>
+                    <p className="description">
+                      {" "}
+                       <strong> Description: </strong>
+                      Designed and executed global social media selling strategy
+                      across 125,000 followers on Store's Website, Amazon, Ebay,
+                      Etsy, Instagram, and Facebook. Regularly used technologies
+                      to sell product on store website; Wordpress, Shopify,
+                      BigCommerce, Google Analytics, Semrush and GoDaddy.
+                      Technologies used regularly to create social media posts,
+                      videos and blogs: Adobe Photoshop, Adobe Premiere Pro,
+                      Final Cut Pro, DaVinci Resolve, Microsoft Office 365 and
+                      Canva.
+                    </p>
                   </Card.Text>
                 </div>
               </Card.Body>
--- src/Pages/Experience.js
+++ src/Pages/Experience.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Title from "../Components/Title";
 import ExperienceComponent from "../Components/Experience"
+import hereMood from "../img/experience/Here.png";
 function Experience() {
   return (
     <div>
       <div className="title">
         <Title title={"Experience"} span={"Experience"} />
-        <ExperienceComponent/>
+        <ExperienceComponent img={hereMood} title="Social Media &amp; E-Commerce Manager" />
       </div>
     </div>
   );
